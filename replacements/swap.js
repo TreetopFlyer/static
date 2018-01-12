8a9e7dc6372190900967a675dda3bfbcf35a31be
SethTFL <33667649+SethTFL@users.noreply.github.com>
2018-01-12 09:37:34 -0500

    Create swap.js

---
+++ replacements/swap.js
@@ -0,0 +1,2 @@
+document.body.style.backgroundImage = "url(https://sethtfl.github.io/static/replacements/watercolor.jpg)";
+document.getElementsByClassName("subtitle")[0].src = "https://sethtfl.github.io/static/replacements/basics2018_logo_reverse.svg";
